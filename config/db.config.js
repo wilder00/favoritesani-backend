64490fe49115ca287ecd03d2ce92d475d44bd283
Wilder Trujillo <nadie437@gmail.com>
2022-12-11 17:09:46 -0500

    Agregar connection a base de datos

--- config/db.config.js
+++ config/db.config.js
@@ -1,16 +1,28 @@
-
+const mysql = require('mysql2');
+const {
+  DB_HOST: dbHost,
+  DB_PORT: dbPort,
+  DB_USER: dbUser,
+  DB_NAME: dbName,
+  DB_PASSWORD: dbPassword,
+} = process.env
+const connection = mysql.createConnection({
+  host     : dbHost,
+  port     : dbPort,
+  user     : dbUser,
+  database : dbName,
+  password : dbPassword,
+});
 // const dbPassword = process.env.DB_PASSWORD
-
-const dbConnection = async () =>{
-  try{
-    
-    console.log("test db config");
-  }catch(error){
-    console.log(error);
-    throw new Error("error a la hora de inicializar db")
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting Mysql: ' + err.stack);
+    return;
   }
-}
+
+  console.log('connected Mysql as id ' + connection.threadId);
+});
 
 module.exports = {
-  dbConnection
+  connection
 }
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./app')
 const port = process.env.PORT || 3000
 const database = require('./config/db.config')
 
-database.dbConnection().catch((error)=>{console.log(error)})
+
 
 app.listen(port, ()=>{
   console.log(`Escuchando en el puerto ${port}`)
